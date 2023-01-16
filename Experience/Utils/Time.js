dac44e798a315bb1680379c439141314fd2483be
Rajan Kumar <rajanrk5151@gmail.com>
2023-01-16 14:45:52 +0530

    first commit

---
+++ Experience/Utils/Time.js
@@ -0,0 +1,20 @@
+import {EventEmitter} from "events";
+
+export default class Time extends EventEmitter{
+    constructor(){
+        this.start = Date.now();
+        this.current = this.start;
+        this.elapsed = 0;
+        this.delta = 16;
+        this.update();
+    }
+
+    update(){
+        const currentTime = Date.now();
+        this.delta = currentTime - this.current;
+        this.current = currentTime;
+        this.elapsed = this.current - this.start;
+
+        window.requestAnimationFrame(() => this.update());
+    }
+}
